7052a56e7c34de398ebbfc5dd6c1c9df3f61164b
elzzup <guild0105@gmail.com>
2015-01-03 02:46:06 +0900

    create search form

---
+++ application/views/psychopassform.php
@@ -0,0 +1,16 @@
+<div class="row panel border">
+    <div class="small-11 small-centered medium-10 medium-push-1">
+        <form mehtod="GET" action="<?= base_url() ?>">
+            <div class="row">
+                <div class="medium-8 columns">
+                    <label>ユーザID
+                        <input type="text" placeholder="arzzup" name="sn">
+                    </label>
+                </div>
+                <div class="medium-4 columns">
+                    <input class="button small" type="submit" value="ドミネーターを向ける">
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
